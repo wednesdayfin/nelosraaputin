4d6b9337d9af564a75cd1285d7500b3f11b7b8d6
wednesdayfin <raimovormisto86@gmail.com>
2016-03-03 21:20:17 +0200

    Update raaputin.py

--- raaputin.py
+++ raaputin.py
@@ -1,4 +1,6 @@
 import scrapy
+import random
+import pyttsx
 
 class DmozSpider(scrapy.Spider):
     name = "nelonen"
@@ -6,14 +8,29 @@ class DmozSpider(scrapy.Spider):
     start_urls = [
         "http://boards.4chan.org/a/"
     ]
+    engine = pyttsx.init()
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate - 50)
+    
+    #engine.say('Progress isn\'t made by early risers. It\'s made by lazy men trying to find easier ways to do something.')
+    #engine.runAndWait()
     
 
     def parse(self, response):
+    	num = random.randint(0,9)
+    	
+
         for sel in response.xpath('//form/div/div/div/div'):
         	title = sel.xpath('div/span[1]/text()').extract()
-        	print title
+        	if (len(title) > 1):
+        		print title[1]
+        		
+        print "\nnumeroksi arpoutui: %d\n" % num
         	
 	for i in response.xpath('//form/div/div/div/div'):
 		posti = i.xpath('blockquote/text()').extract()
-		print posti
+		if (len(posti) > 1):
+			print ' '.join(posti)
 
+##to do:
+#text-to-speech
